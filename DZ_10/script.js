ee80a25aa615215521eee6db112d24f485a01abe
Roman <starsetphenomenon@gmail.com>
2022-05-28 18:23:00 +0300

    negative numbers added

--- DZ_10/script.js
+++ DZ_10/script.js
@@ -1,8 +1,10 @@
 function Fib(n) {
     if (n === 0 || n === 1) {
         return n;
+    } else if (n < 0) {
+        return ((-1) ** (n + 1)) * Fib(-n);
     }
     return Fib(n - 1) + Fib(n - 2);
 }
 
-console.log(Fib(7));
+console.log(Fib(-6));
