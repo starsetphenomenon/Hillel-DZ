584fde30333f98b8ea20f85658f1af467e880c3e
Roman <starsetphenomenon@gmail.com>
2022-05-28 18:23:49 +0300

    neg num added

--- DZ_10/script.js
+++ DZ_10/script.js
@@ -1,10 +1,10 @@
-function Fib(n) {
+function getFibonacciNumber(n) {
     if (n === 0 || n === 1) {
         return n;
     } else if (n < 0) {
-        return ((-1) ** (n + 1)) * Fib(-n);
+        return ((-1) ** (n + 1)) * getFibonacciNumber(-n);
     }
-    return Fib(n - 1) + Fib(n - 2);
+    return getFibonacciNumber(n - 1) + getFibonacciNumber(n - 2);
 }
 
-console.log(Fib(-6));
+console.log(getFibonacciNumber(-6));
