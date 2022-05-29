42069c8eff8525200f49166c0294ec64d099cf7f
Roman <starsetphenomenon@gmail.com>
2022-05-29 18:01:35 +0300

    reduce added

--- DZ_9/script.js
+++ DZ_9/script.js
@@ -3,17 +3,21 @@ const studentsList = studentsMock.getStudentList(STUDENTS_AMOUNT);
 console.log(studentsList);
 
 function getSumOfNumbers(arr) {
-    let result = 0;
-    arr.forEach(elem => {
-        result += elem;
+    let result = arr.reduce((res, elem) => {
+        return res += elem;
     });
     return result;
 }
 
 function getGroupAvg(students) {
-    let studentAvgPoints = students.map(elem => Math.floor(getSumOfNumbers(elem.marks) / elem.marks.length));
-    let result = Math.floor(getSumOfNumbers(studentAvgPoints) / studentAvgPoints.length);
-    return 'Group average point is: ' + result;
+    let studentAvgPoints = students.reduce((res, elem, ind, arr) => {
+        res += Math.floor(getSumOfNumbers(elem.marks) / elem.marks.length);
+        if (ind === arr.length - 1) {
+            return Math.floor(res / arr.length);
+        }
+        return res;
+    }, 0);
+    return 'Group average point is: ' + studentAvgPoints;
 }
 
 console.log(getGroupAvg(studentsList));
