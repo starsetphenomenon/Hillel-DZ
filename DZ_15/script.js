ee4f25b248fb280a211a3f2ebc98c7c7b2e47d38
Roman <starsetphenomenon@gmail.com>
2022-06-25 17:32:21 +0300

    done

--- DZ_15/script.js
+++ DZ_15/script.js
@@ -1,28 +1,28 @@
 function calc(num) {
-    let result;
+    let result = 0;
     let final = num;
     math = {
         add: function (val) {
             result = +(num + val).toFixed(1);
-            final = +(final + result).toFixed(1);
+            final = +(final + result - num).toFixed(1);
             return result;
         },
         sub: function (val) {
             result = +(num - val).toFixed(1);
-            final = +(final + result).toFixed(1);
+            final = +(final + result - num).toFixed(1);
             return result;
         },
         div: function (val) {
             result = +(num / val).toFixed(1);
-            final = +(final + result).toFixed(1);
+            final = +(final / val).toFixed(1);
             return result;
         },
         mult: function (val) {
             result = +(num * val).toFixed(1);
-            final = +(final + result).toFixed(1);
+            final = +(final * num).toFixed(1);
             return result;
         },
-        primary: function (val) {
+        getPrimary: function (val) {
             return num;
         },
         getResult: function (val) {
@@ -34,8 +34,10 @@ function calc(num) {
 
 const first = calc(4);
 
+
 console.log(first.add(33));
 console.log(first.sub(12));
 console.log(first.div(2));
 console.log(first.mult(4));
+console.log(first.getPrimary());
 console.log(first.getResult());
