38d80d9c84061c7ceabe9ca6703af6ed32863932
Roman <starsetphenomenon@gmail.com>
2022-07-22 19:36:45 +0300

    analog added

--- DZ_19-time/script.js
+++ DZ_19-time/script.js
@@ -88,3 +88,44 @@ const updateMyTime = function () {
 };
 
 let changeTime = setInterval(updateMyTime, 1000);
+
+
+// ANALOG WATCH CODE STARTS HERE ~~
+
+const secondsArrow = document.getElementById('secondsArrow');
+const minutesArrow = document.getElementById('minutesArrow');
+const hoursArrow = document.getElementById('hoursArrow');
+
+const refreshArrow = function (arrowName) {
+    let date = new Date();
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let angle = null;
+
+    switch (arrowName) {
+        case 'seconds':
+            angle = (seconds * 360) / 60;
+            secondsArrow.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+            break;
+        case 'minutes':
+            angle = (minutes * 360) / 60;
+            minutesArrow.style.transform = `translateX(-50%) rotate(${angle}deg)`;
+            break;
+        case 'hours':
+            angle = (hours * 360) / 12;
+            hoursArrow.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+            break;
+        default:
+            console.log('Wrong input!');
+            break;
+    }
+};
+
+const refreshAllArrows = function () {
+    refreshArrow('minutes');
+    refreshArrow('seconds');
+    refreshArrow('hours');
+};
+
+let analogInterval = setInterval(refreshAllArrows, 1000);
